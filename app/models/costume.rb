4c025326add60594b318a18b4c5e645c909c6eed
Andrew Bernier <a6bernier@gmail.com>
2018-11-14 20:59:22 +0000

    automatically backed up by learn

--- app/models/costume.rb
+++ app/models/costume.rb
@@ -3,4 +3,13 @@
 
 class Costume < ActiveRecord::Base
 
+  def change
+    create_table :costumes do |t|
+      t.string :name
+      t.string :price
+      t.string :size
+      t.string :image_url
+    end
+  end
+
 end
