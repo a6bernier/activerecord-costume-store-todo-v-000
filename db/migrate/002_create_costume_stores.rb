205d4206359a3896e657b426ae69988008a5a1a2
Andrew Bernier <a6bernier@gmail.com>
2018-11-14 21:32:02 +0000

    automatically backed up by learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -2,6 +2,15 @@
 
 class CostumeStores
 
-
+  def change
+    create_table :costume_stores do |t|
+      t.string :name
+      t.string :location
+      t.integer :costume_inventory
+      t.boolean :still_in_business
+      t.datetime :opening_time
+      t.datetime :closing_time
+      t.timestamps
+    end
 
 end
